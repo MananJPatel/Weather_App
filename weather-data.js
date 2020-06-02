961f51afa72ee21d4001bcc760bf0889eeb69394
Manan Patel <mjpatel@umass.edu>
2020-06-02 19:56:25 -0400

    final commit

--- weather-data.js
+++ weather-data.js
@@ -5,3 +5,12 @@
 //     this.description = description;
 //     this._temperature = '';
 // }
+
+// // Object.defineProperty(Weather.prototype, 'temperature', {
+// //     get: function() {
+// //         return this._temperature;
+// //     },
+// //     set: function(value) {
+// //         this._temperature = (value * 1.8 + 32).toFixed(2) + 'F.';
+// //     }
+// // });
